f68391831bb3464c0a36e9197189a5913491ac7a
chuang-tw <chuang.15407@yahoo.com.tw>
2020-02-28 18:26:55 +0800

    add new

--- products.py
+++ products.py
@@ -8,5 +8,13 @@ while True:
 	p.append(name)
 	p.append(price)
 	products.append(p)
-	
+
 print(products)
+
+for p in products:
+	print(p[0], '的價格是', p[1])
+
+with open('price.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
+
